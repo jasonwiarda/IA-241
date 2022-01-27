186465e94dcfa99206f1c3172c8c970cb9009bdd
Jason Wiarda <jasonwiarda@gmail.com>
2022-01-27 18:44:00 +0000

    my lab 2

---
+++ Lab2.py
@@ -0,0 +1,48 @@
+'''
+Lab 2
+'''
+
+
+#3.1
+my_name = "Jason"
+
+print (my_name)
+
+#3.2
+my_id = "123"
+
+print (my_id)
+
+#3.3
+
+# _123=my_id
+my_id = your_id=123
+print(my_id)
+print(your_id)
+
+#3.4
+
+my_id_str = "123"
+print(my_id_str)
+
+#3.5
+
+# print(my_name+my_id)
+
+#3.6
+
+print(my_name + my_id_str)
+
+#3.7
+
+print(my_name*3)
+
+
+#3.8
+
+print("hello, world. This is my first python string.".split('.'))
+
+#3.9
+
+message = "Jason's ID is 123"
+print(message)
